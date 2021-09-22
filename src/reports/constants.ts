e01d20e309e9f3498ac1adaf0e21d31add8111c5
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2021-09-22 23:32:27 +0300

    update evidences and responseActions lists

--- src/reports/constants.ts
+++ src/reports/constants.ts
@@ -4,14 +4,18 @@ export const evidences = {
   102: 'Cleared areas',
   103: 'Logging equipment',
   104: 'Loggers at site',
-  105: 'Other'
+  105: 'Illegal camps',
+  106: 'Fired/burned areas',
+  107: 'Evidence of poaching'
 }
 
 export const responseActions = {
   200: 'None',
-  201: 'Confiscated equipment',
-  202: 'Collected evidence',
-  203: 'Submitted report of incident',
-  204: 'Planning to patrol area again in near future',
-  205: 'Other'
+  201: 'Collected evidence',
+  202: 'Issue a warning',
+  203: 'Confiscated equipment',
+  204: 'Issue a fine',
+  205: 'Arrests',
+  206: 'Planning to come back with security enforcement',
+  207: 'Other'
 }
