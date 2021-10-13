e08d0190aa052d0bf3addac1da40f5557f5e80fc
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2021-10-13 15:19:33 +0300

    add log for new message

--- src/events/consumer.ts
+++ src/events/consumer.ts
@@ -8,6 +8,7 @@ if (process.env.EVENTS_SQS_QUEUE_ENABLED === 'true') {
   console.log(`Subsribing to SQS topic "${topic}"`)
   messageQueue.subscribe(topic, async (data: EventSQSMessage) => {
     try {
+      console.log('New message with event received', data)
       await createEvent(data)
     } catch (e) {
       console.error('Error creating event', e)
